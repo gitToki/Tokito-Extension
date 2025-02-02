15990ab6b621a557700e06b9b38cfb24ea4515b9
Toki <yassineferhane69001@hotmail.com>
2025-02-02 16:43:17 +0100

    project frontend set up

--- tokito-frontend/vite.config.ts
+++ tokito-frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        // Copy past the manifest and icons on dist
+        { src: 'manifest.json', dest: '' }, 
+        { src: 'icons/*', dest: 'icons' }, 
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+  },
+});
